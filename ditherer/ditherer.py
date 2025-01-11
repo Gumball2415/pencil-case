cc6cb2332711a66bd34c26f4c68be494bcde1c9a
Persune <gumball2415@gmail.com>
2025-01-12 02:35:32 +0800

    ditherer: add `deltae` argument, refactor some

--- ditherer/ditherer.py
+++ ditherer/ditherer.py
@@ -6,36 +6,32 @@ import numpy as np
 import sys, argparse, os
 from PIL import Image, ImageColor, ImagePalette
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 
 # http://brucelindbloom.com/index.html?Math.html
+srgb_to_xyz_M = np.array([
+    [0.4124564, 0.3575761, 0.1804375],
+    [0.2126729, 0.7151522, 0.0721750],
+    [0.0193339, 0.1191920, 0.9503041]])
+
 def srgb_to_xyz(rgb_array):
-    M = np.array([
-        [0.4124564, 0.3575761, 0.1804375],
-        [0.2126729, 0.7151522, 0.0721750],
-        [0.0193339, 0.1191920, 0.9503041]])
-    return np.linalg.matmul(M,rgb_array)
+    return np.linalg.matmul(srgb_to_xyz_M,rgb_array)
+
+# https://en.wikipedia.org/wiki/Standard_illuminant#D65_values
+d65_x = 0.31272
+d65_y = 0.32903
+d65_Y = 100
+
+X_r = (d65_x*d65_Y) / d65_y
+Y_r = d65_Y
+Z_r = ((1-d65_x-d65_y) * d65_Y)/d65_y
+epsilon = 216/24389
+kappa = 24389/27
 
 def xyz_to_lab(xyz_array):
-    X = xyz_array[0]
-    Y = xyz_array[1]
-    Z = xyz_array[2]
-
-    # https://en.wikipedia.org/wiki/Standard_illuminant#D65_values
-    d65_x = 0.31272
-    d65_y = 0.32903
-    d65_Y = 100
-
-    X_r = (d65_x*d65_Y) / d65_y
-    Y_r = d65_Y
-    Z_r = ((1-d65_x-d65_y) * d65_Y)/d65_y
-
-    x_r = X/X_r
-    y_r = Y/Y_r
-    z_r = Z/Z_r
-    
-    epsilon = 216/24389
-    kappa = 24389/27
+    x_r = xyz_array[0]/X_r
+    y_r = xyz_array[1]/Y_r
+    z_r = xyz_array[2]/Z_r
 
     f_x = np.cbrt(x_r) if x_r > epsilon else (kappa*x_r+16)/116
     f_y = np.cbrt(y_r) if y_r > epsilon else (kappa*y_r+16)/116
@@ -47,58 +43,58 @@ def xyz_to_lab(xyz_array):
 
     return np.array([L, a, b])
 
-def lab_to_lch(lab_array):
-    L = lab_array[0]
-    a = lab_array[1]
-    b = lab_array[2]
-    
-    C = np.sqrt(a**2+b**2)
-    h = np.atan2(b,a)
-    
-    return np.array([L, C, h])
-
 # finds index with shortest CIE94 distance
 # https://bisqwit.iki.fi/story/howto/dither/jy/#Appendix%203ColorComparisons
-# http://brucelindbloom.com/index.html?Math.html
-def closest_CIE94_index(px_in, palette, textile=False):
-    px_in = np.array(px_in)
-    palette = np.array(palette)
-    # convert colors to CIELAB
-    px_Lab = xyz_to_lab(srgb_to_xyz(px_in))
-    pal_Lab = [xyz_to_lab(srgb_to_xyz(pal)) for pal in palette]
+# http://brucelindbloom.com/Eqn_DeltaE_CIE94.html
+def closest_CIE94_index(px_in, palette, args=None):
+    px_in = np.array(xyz_to_lab(srgb_to_xyz(px_in)))
+    palette = np.array([xyz_to_lab(srgb_to_xyz(pal)) for pal in palette])
     distances = []
 
-    for pal in pal_Lab:
-        delta_L = pal[0] - px_Lab[0]
-        delta_a = pal[1] - px_Lab[1]
-        delta_b = pal[2] - px_Lab[2]
-        C_1 = np.sqrt(pal[1]**2+pal[2]**2)
-        C_2 = np.sqrt(px_Lab[1]**2+px_Lab[2]**2)
-        C_12 = np.sqrt(C_1*C_2)
+    K_L = 2 if args.CIE94_tex else 1
+    # K_C and K_H is 1 by default
+    K_1 = 0.048 if args.CIE94_tex else 0.045
+    K_2 = 0.014 if args.CIE94_tex else 0.015
+
+    for pal in palette:
+        # convert colors to LC
+        delta_L = pal[0] - px_in[0]
+        delta_a = pal[1] - px_in[1]
+        delta_b = pal[2] - px_in[2]
+
+        C_1 = np.sqrt(pal[1]**2     +   pal[2]**2)
+        C_2 = np.sqrt(px_in[1]**2   +   px_in[2]**2)
         delta_C = C_1 - C_2
-        delta_Hsqr = delta_a**2 + delta_b**2 - delta_C**2
-        K_1 = 0.048 if textile else 0.045
-        K_2 = 0.014 if textile else 0.015
-        k_L = 2 if textile else 1
+    
+        # using C_12 because of bisqwit
+        C_12 = np.sqrt(C_1*C_2)
+        # S_L = 1
         S_C = 1 + K_1*C_12
         S_H = 1 + K_2*C_12
+
+        # H is compared in a different way than just euclidean distance
+        # removed sqrt for optimization
+        delta_Hsqr = delta_a**2 + delta_b**2 - delta_C**2
+
         delta_E = np.sqrt(
-            (delta_L**2/k_L**2)+(delta_C**2/S_C**2)+(delta_Hsqr/S_H**2)
+            (delta_L**2/K_L**2)+(delta_C**2/S_C**2)+(delta_Hsqr/S_H**2)
         )
+
         distances.append(delta_E)
 
     distances = np.array(distances)
-    index_of_smallest = np.where(distances==np.amin(distances))[0][0]
+    index_of_smallest = np.where(distances==np.amin(distances))
     return index_of_smallest
 
-# finds index of closest color
-def closest_index(px_in, palette):
-    px_in = np.array(px_in)
-    palette = np.array(palette)
-
+def closest_index(px_in, palette, args=None):
     distances = np.sqrt(np.sum((palette-px_in)**2,axis=1))
-    index_of_smallest = np.where(distances==np.amin(distances))[0][0]
+    index_of_smallest = np.where(distances==np.amin(distances))
     return index_of_smallest
+
+deltae_func = {
+    "sRGB": closest_index,
+    "CIE94": closest_CIE94_index,
+}
 
 def main(argv=None):
     def parse_argv(argv):
@@ -106,9 +102,14 @@ def main(argv=None):
             description="Image ditherer",
             epilog="version " + VERSION)
         parser.add_argument(
-            "-CIE94",
-            help="compares colors in CIE94 Delta-E. default is False",
-            action="store_true")
+            "--deltae",
+            type = str,
+            help = "Method of delta E, or finding closest color. default is \"sRGB\".",
+            choices=[
+                "sRGB",
+                "CIE94"
+            ],
+            default = "sRGB")
         parser.add_argument(
             "-gam",
             "--gamma",
@@ -140,6 +141,8 @@ def main(argv=None):
             help="output .png file")    
         return parser.parse_args(argv[1:])
     args = parse_argv(argv or sys.argv)
+
+    deltae = deltae_func[args.deltae]
     
     palette = np.array([ImageColor.getrgb(color) for color in args.palette], dtype=np.float64)
     # flatten palette?
@@ -148,33 +151,38 @@ def main(argv=None):
     with Image.open(args.mask) as immask:
         if immask.mode != "L":
             sys.exit("dither mask is not 8-bit grayscale!")
+
         dithermask = np.array(immask, dtype=np.float64)
+        n = dithermask.shape[0]
+
         with Image.open(args.input) as im:
             # convert im to RGB only
             im = im.convert(mode="RGB")
             imarr = np.array(im, dtype=np.float64)
             imout = np.zeros(imarr.shape, dtype=np.float64)
+
             # rescale to 0-1
             imarr /= 255.0
             palette /= 255.0
             dithermask /= 255.0
-            n = dithermask.shape[0]
-            # apply 2.2 gamma eotf to image before dither
+
+            # convert to linear light
+            # apply eotf gamma to image before dither
             for y in range(imarr.shape[0]):
                 for x in range(imarr.shape[1]):
                     for c in range(imarr.shape[2]):
                         imarr[y,x,c] = min(max(imarr[y,x,c]**(args.gamma), 0.0), 1.0)
-            # standard ordered dithering
+
+            # modified ordered dithering
             # https://bisqwit.iki.fi/story/howto/dither/jy/#Algorithms
-            for y in range(imarr.shape[0]):
-                for x in range(imarr.shape[1]):
+            for y in range(imout.shape[0]):
+                for x in range(imout.shape[1]):
                     px_in = imarr[y,x]
                     px_mask = dithermask[(y%n),(x%n)]
-                    px_attempt = px_in + px_mask * args.threshold
-                    index = closest_CIE94_index(px_attempt, palette, args.CIE94_tex) if args.CIE94 else closest_index(px_attempt, palette)
+                    px_attempt = px_in + (px_mask * args.threshold)
+                    index = deltae(px_attempt, palette, args)
                     imout[y,x] = palette[index]
-                    
-            # closest_color([0.5,0.5,0], palette)
+
             imout = np.uint8(np.around(imout*255))
             pil_image = Image.fromarray(imout)
             pil_image.save(args.output, format="PNG", optimize=True)
