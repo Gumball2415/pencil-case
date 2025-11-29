2bb114d2d5d98a0c263de28dae1810a340eedfb0
Persune <gumball2415@gmail.com>
2025-11-29 22:30:51 +0800

    ffmpeg_stuff: update scripts
    - Increase bitrate in opus transcoder
    - Increase bitrate in mp3lame bitcrusher

--- ffmpeg_stuff/mp3 bitrate crush encode.bat
+++ ffmpeg_stuff/mp3 bitrate crush encode.bat
@@ -1,3 +1,3 @@
-ffmpeg -y -i %1 -vn -ar 32000 -c:a libmp3lame -b:a 8k -abr 1 "%~dp1%~n1_mp3_bitcrush.mp3"
+ffmpeg -y -i %1 -vn -ar 8000 -c:a libmp3lame -b:a 12k -abr 1 "%~dp1%~n1_mp3_bitcrush.mp3"
 echo 
 pause
--- ffmpeg_stuff/opus encode.bat
+++ ffmpeg_stuff/opus encode.bat
@@ -1,3 +1,3 @@
-ffmpeg -y -i %1 -vn -c:a libopus -b:a 96k -vbr on -compression_level 10 -frame_duration 120 "%~dp1%~n1_reencode.opus"
+ffmpeg -y -i %1 -vn -c:a libopus -b:a 192k -vbr on -compression_level 10 -frame_duration 120 "%~dp1%~n1_reencode.opus"
 echo 
 pause
