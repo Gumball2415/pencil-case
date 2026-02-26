a4f8ef106d7f8939ce322aaff266d06009d4c5f1
Persune <gumball2415@gmail.com>
2026-02-26 18:57:24 +0800

    composite-filter: save .tbc

--- composite-filter/encode-decode.sh
+++ composite-filter/encode-decode.sh
@@ -1,22 +1,29 @@
 # uses ld-tools suite and vhs-decode from https://github.com/oyvindln/vhs-decode
+
+dir=$(dirname "$1")
+base=$(basename "$1")
+framedir=${base%.*}"_frames"
+
+
+echo $dir
+echo $base
+echo $framedir
+
 python3 composite_filter.py "$@"
 cvbs-decode -A --overwrite --frequency 13.5MHz -n -t 10\
     "${1%.*}_13.5MHz_16_bit.cvbs" \
     "${1%.*}_13.5MHz_16_bit_decode"
 ld-process-vbi --input-json "${1%.*}_13.5MHz_16_bit_decode.tbc.json" -n -t 10\
-    "${1%.*}_13.5MHz_16_bit_decode.tbc"
-
-dir=$(dirname $1)
-base=$(basename $1)
-framedir=${base%.*}"_frames"
+        "${1%.*}_13.5MHz_16_bit_decode.tbc"
 
 mkdir "$dir/$framedir"
+
 
 ld-dropout-correct -o -t 10\
     --input-json "${1%.*}_13.5MHz_16_bit_decode.tbc.json" \
     --output-json "${1%.*}_13.5MHz_16_bit_decode.tbc.json" \
     "${1%.*}_13.5MHz_16_bit_decode.tbc" - |\
-ld-chroma-decoder -t 10 - \
+ld-chroma-decoder -t 10 "${1%.*}_13.5MHz_16_bit_decode.tbc" \
     --input-json "${1%.*}_13.5MHz_16_bit_decode.tbc.json" \
     --decoder ntsc2d --ntsc-phase-comp \
     -p y4m |\
